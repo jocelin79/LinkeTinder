76c6ade13322856616cc1df35a70671743e4e479
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-09-28 13:34:14 -0300

    atualização

--- src/main/groovy/Candidato.groovy
+++ src/main/groovy/Candidato.groovy
@@ -1,9 +1,5 @@
 @groovy.transform.ToString
 class Candidato extends Pessoa{
-    String nome, email, cpf
-    Integer idade
-    String estado
-    Integer cep
-    String descricao
-    List<String> competencias
+    String nome, sobrenome, email, cpf, pais_onde_reside, cep, descricao, senha;
+    Date data_nascimento = new Date();
 }
