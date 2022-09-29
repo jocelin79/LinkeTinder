ef0d19a0648daaedfb348a81bd62806b1d9bfa45
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-09-29 15:51:11 -0300

    Create

---
+++ src/main/groovy/VagaDAO.groovy
@@ -0,0 +1,46 @@
+import groovy.sql.Sql
+
+class VagaDAO {
+ 
+  String url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/linketinderdb'
+  String user = 'postgres'
+  String password = 'postgres'
+  String driver = 'org.postgresql.Driver'
+  Sql sql = Sql.newInstance(url, user, password, driver)
+
+  List<Vaga> listar() {
+    List<Vaga> retorno = new ArrayList<>();
+    sql.query('SELECT * FROM vaga') { resultSet ->
+      while (resultSet.next()) {
+        Vaga vaga = new Vaga()
+        vaga.setId(resultSet.getInt("id"))
+        vaga.setDescricao(resultSet.getString("descricao"))
+        vaga.setLocal_da_vaga(resultSet.getString("local_da_vaga"))
+        vaga.setIdEmpresa(resultSet.getInt("idEmpresa"))
+        retorno.add(vaga)
+      }
+    }
+    return retorno
+  }
+  
+  boolean inserir(Vaga vaga) {
+   String insertSql = 'INSERT INTO vaga(descricao, local_da_vaga, idEmpresa) VALUES(?, ?, ?)'
+   def params = [vaga.getDescricao(), vaga.getLocal_da_vaga(), vaga.getIdEmpresa()]
+   sql.execute insertSql, params
+   return true;
+  }
+ 
+  boolean alterar(Vaga vaga) {
+   String updateSql = 'UPDATE vaga SET descricao=?, local_da_vaga=?, idEmpresa=? WHERE id=?'
+   def params = [vaga.getDescricao(), vaga.getLocal_da_vaga(), vaga.getIdEmpresa(), vaga.getId()]
+   sql.execute updateSql, params
+   return true;
+  }
+ 
+  boolean remover(Integer id) {
+   String deleteSql = 'DELETE FROM vaga WHERE id=?'
+   def params = vaga.getId()
+   sql.execute deleteSql, params
+   return true;
+  }
+}
