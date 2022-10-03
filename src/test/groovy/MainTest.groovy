12398fb82d60dcaa2604b88de4b666f189d988e0
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-10-03 16:11:26 -0300

    Create

---
+++ src/test/groovy/MainTest.groovy
@@ -0,0 +1,9 @@
+import org.junit.Teste;
+import static org.junit.Assert.*;
+
+public class MainTeste {
+  @Test public void testAppHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+}
